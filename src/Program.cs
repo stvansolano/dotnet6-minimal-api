229357c21ae39550f2f5bd8454173545583a38ff
Esteban Solano Granados <stvansolano@outlook.com>
2021-12-05 21:28:48 +0000

    Added Todo api playground

--- src/Program.cs
+++ src/Program.cs
@@ -1,27 +1,97 @@
+// Based on https://github.com/DamianEdwards/MinimalApiPlayground/blob/main/src/Todo.EFCore/Program.cs
+
+using System.ComponentModel.DataAnnotations;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
+
+var connectionString = builder.Configuration.GetConnectionString("TodoDb") ?? "Data Source=../todos.db";
+builder.Services.AddSqlite<TodoDb>(connectionString)
+                .AddDatabaseDeveloperPageExceptionFilter();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
 var app = builder.Build();
 
-var summaries = new[]
-{
-    "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-};
+await EnsureDb(app.Services, app.Logger);
 
-app.MapGet("/", () =>
+if (!app.Environment.IsDevelopment())
 {
-    var forecast = Enumerable.Range(1, 5).Select(index =>
-       new WeatherForecast
-       (
-           DateTime.Now.AddDays(index),
-           Random.Shared.Next(-20, 55),
-           summaries[Random.Shared.Next(summaries.Length)]
-       ))
-        .ToArray();
-    return forecast;
+    app.UseExceptionHandler("/error");
+}
+
+app.MapGet("/error", () => Results.Problem("An error occurred.", statusCode: 500))
+   .ExcludeFromDescription();
+
+app.MapSwagger();
+app.UseSwaggerUI();
+
+app.MapGet("/", () => "Hello World!");
+
+app.MapGet("/api/todos", async (TodoDb db, ILogger<IConfiguration> logger) => {
+    var result = await db.Todos.ToListAsync();
+    logger.LogInformation($"Found {result.Count()} records");
+
+    return result;
 });
 
+app.MapGet("/api/todos/{id}", async (int id, TodoDb db) =>
+    await db.Todos.FindAsync(id)
+        is Todo todo
+            ? Results.Ok(todo)
+            : Results.NotFound())
+    .Produces<Todo>(StatusCodes.Status200OK)
+    .Produces(StatusCodes.Status404NotFound);
+
+app.MapPost("/api/todos", async (Todo todo, TodoDb db) =>
+    {
+        db.Todos.Add(todo);
+        await db.SaveChangesAsync();
+
+        return Results.Created($"/todos/{todo.Id}", todo);
+    })
+    .ProducesValidationProblem()
+    .Produces<Todo>(StatusCodes.Status201Created);
+
+app.MapDelete("/todos/{id}", async (int id, TodoDb db) =>
+    {
+        if (await db.Todos.FindAsync(id) is Todo todo)
+        {
+            db.Todos.Remove(todo);
+            await db.SaveChangesAsync();
+            return Results.Ok(todo);
+        }
+
+        return Results.NotFound();
+    })
+    .Produces(StatusCodes.Status204NoContent)
+    .Produces(StatusCodes.Status404NotFound);
+
+// dotnet run --urls "http://*:80"
 app.Run();
 
-internal record WeatherForecast(DateTime Date, int TemperatureC, string? Summary)
+async Task EnsureDb(IServiceProvider services, ILogger logger)
 {
-    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+    logger.LogInformation("Ensuring database exists and is up to date at connection string '{connectionString}'", connectionString);
+
+    using var db = services.CreateScope().ServiceProvider.GetRequiredService<TodoDb>();
+    
+    await db.Database.EnsureCreatedAsync();
+    await db.Database.MigrateAsync();
+}
+
+class Todo
+{
+    public int Id { get; set; }
+    [Required]
+    public string? Title { get; set; }
+    public bool IsComplete { get; set; }
+}
+
+class TodoDb : DbContext
+{
+    public TodoDb(DbContextOptions<TodoDb> options)
+        : base(options) { }
+
+    public DbSet<Todo> Todos => Set<Todo>();
 }
